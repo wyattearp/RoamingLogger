b3ff5bdca836a46ab8641442a3347112f6768a67
Wyatt Neal <wyatt.neal+git@gmail.com>
2010-08-10 08:35:24 -0400

    seems to all work

--- gen/com/htglimited/RoamingLogger/R.java
+++ gen/com/htglimited/RoamingLogger/R.java
@@ -14,8 +14,9 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int btnEmailSend=0x7f050001;
-        public static final int txtViewEmailBody=0x7f050000;
+        public static final int btnEmailSend=0x7f050002;
+        public static final int txtViewEmailBody=0x7f050001;
+        public static final int txtViewResults=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -24,6 +25,7 @@ public final class R {
         public static final int app_name=0x7f040001;
         public static final int btnEmailSend=0x7f040002;
         public static final int hello=0x7f040000;
-        public static final int lblEmailBody=0x7f040003;
+        public static final int txtViewEmailBody=0x7f040003;
+        public static final int txtViewResults=0x7f040004;
     }
 }
--- src/com/htglimited/RoamingLogger/RoamingLogger.java
+++ src/com/htglimited/RoamingLogger/RoamingLogger.java
@@ -1,17 +1,13 @@
 package com.htglimited.RoamingLogger;
 
 import android.app.Activity;
-import android.os.Bundle;
-
-import android.app.Activity;
 import android.content.Context;
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
 import android.telephony.TelephonyManager;
-import android.widget.Button;
-import android.widget.EditText;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class RoamingLogger extends Activity {
@@ -20,14 +16,16 @@ public class RoamingLogger extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        TextView tv = new TextView(this);
+        setContentView(R.layout.main);
+        
+        TextView tv = new TextView(this); //(TextView) findViewById(R.id.txtViewResults);
         Context ctx = this.getApplicationContext();
         TelephonyManager tm = (TelephonyManager) ctx.getSystemService(Context.TELEPHONY_SERVICE);
         Button btnSend;
-        EditText emailBody;
+        TextView emailBody;
         
         btnSend = (Button) findViewById(R.id.btnEmailSend);
-        emailBody = (EditText) findViewById(R.id.txtViewEmailBody);
+        emailBody = (TextView) findViewById(R.id.txtViewEmailBody);
         
         if (btnSend != null) {
         	btnSend.setOnClickListener(new OnClickListener() {
@@ -54,24 +52,20 @@ public class RoamingLogger extends Activity {
                 				+ "Network Country ISO (NETcISO): '" + tm.getNetworkCountryIso() + "'\n"
                 				+ "SIM Country ISO (SIMcISO): '" + tm.getSimCountryIso() + "'\n";
                 
-                tv.setText(myText);
-                //emailBody.setText(myText);
+                emailBody.append(myText);
                 
         }
         
-        this.setContentView(tv);
-        //this.addContentView(tv, null);
-        //this.addContentView(emailBody, null);
     }
     
 	/**
 	 * Method to send email
 	 */
 	protected void sendEmail() {
-		TextView tv = new TextView(this);
+		
+		TextView emailBody = (TextView) findViewById(R.id.txtViewEmailBody);
 		// Setup the recipient in a String array
 		String[] mailto = { "wyatt.neal@gmail.com" };
-		//String[] ccto = { "somecc@somedomain.com" };
 		// Create a new Intent to send messages
 		Intent sendIntent = new Intent(Intent.ACTION_SEND);
 		// Add attributes to the intent
@@ -79,7 +73,7 @@ public class RoamingLogger extends Activity {
 		//sendIntent.putExtra(Intent.EXTRA_CC, ccto);
 		sendIntent.putExtra(Intent.EXTRA_SUBJECT, "Roaming Logger Result"
 				.toString());
-		sendIntent.putExtra(Intent.EXTRA_TEXT, tv.getText().toString());
+		sendIntent.putExtra(Intent.EXTRA_TEXT, emailBody.getText().toString());
 		// sendIntent.setType("message/rfc822");
 		sendIntent.setType("text/plain");
 		startActivity(Intent.createChooser(sendIntent, "MySendMail"));
